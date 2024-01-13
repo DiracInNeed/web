b7df0880c00da829fee0dfc01d62c36bea03af11
DiracInNeed <122070563+DiracInNeed@users.noreply.github.com>
2024-01-13 10:55:03 +0000

    +

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,28 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="app">
+      <h1>Hello World!</h1>
+  </div>
+  <people/>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<script lang="ts">
+  import people from './components/people.vue';
+  export default {
+      name: 'App',
+      components: { people }
   }
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
+<style>
+  .app {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 100%;
+      width: 100%;
+      background-color: #98afde;
+      border-radius: 5dvh;
+      opacity: 0.8;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ vue-project/src/components/people.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">年龄+1</button>
+        <button @click="showTel">点我查看联系方式</button>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'App',
+    data() {
+        return {
+            name: '张三',
+            age: 18,
+            tel: '13888888888'
+        }
+    },
+    methods: {
+        changeName() {
+            this.name = 'zhang-san'
+        },
+        changeAge() {
+            this.age += 1
+        },
+        showTel() {
+            alert(this.tel)
+        }
+    },
+}
+</script>
+
+<style>
+    .person {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        width: 100%;
+        background-color: #da9ff2;
+        border-radius: 5dvh;
+        opacity: 0.8;
+    }
+</style>
